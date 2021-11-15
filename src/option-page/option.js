84f3b2a5eefa924b8d08a91884c56aff6619e202
brunoannunciato <brunobannunciato@gmail.com>
2021-11-15 13:22:45 -0300

    :sparkles: added header

--- src/option-page/option.js
+++ src/option-page/option.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Header from './components/Header'
 import './style.scss'
 
 const App = () => {
   return (
     <div>
-      formfill
+      <Header/>
     </div>
   )
 }
