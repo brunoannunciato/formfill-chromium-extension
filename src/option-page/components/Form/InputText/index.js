83ccee13a14cf6290b83917e3a3212b204cb8533
brunoannunciato <brunobannunciato@gmail.com>
2022-03-08 21:44:07 -0300

    :sparkles: created custom text input

---
+++ src/option-page/components/Form/InputText/index.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+
+import './input-text.scss'
+
+const InputText = ({ label, ...props }) => {
+  const [isFilled, setIsFilled] = useState(false)
+
+  const checkIfIsFilled = (event) => {
+    const element = event.target
+
+    if (element.value.length > 0) {
+      setIsFilled(true)
+      return
+    }
+
+    setIsFilled(false)
+  }
+
+  return (
+    <div className="input-wrapper">
+
+      <input
+        className={`input-wrapper__input-text ${isFilled ? 'input-wrapper__input-text--filled' : ''}`}
+        onInput={ (event) => checkIfIsFilled(event ) }
+        { ...props } 
+      />
+
+      <p className="input-wrapper__label">
+        { label }
+      </p>
+    </div>
+  )
+}
+
+export default InputText
